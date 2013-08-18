df4a4586f7b82f5547a91114fa471f78a592707e
John Gieselmann <johngieselmann@gmail.com>
2013-08-17 21:52:08 -0600

    JohnG. Setup the navigation and home sections.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,46 @@
  * Self executing function on page load.
  */
 (function(window, document, $, undefined){
-    var scroller;
 
-    console.log('loaded');
-    
+    var wind = $(window);
+    var doc = $(document);
 
-})();
+    //adjust some things while scrolling
+    var scrolling = {
+
+        midLine: function() {
+            return parseInt(wind.height() / 2);
+        }
+
+        ,changeColor: function() {
+            var midLine = scrolling.midLine();
+
+            
+        }
+    };
+
+    var nav = {
+        items: null
+
+        ,init: function() {
+            nav.items = $(".js-nav");
+            nav.items.on("click", nav.goTo);
+        }
+        
+        ,goTo: function(e, el) {
+            if (typeof el == "undefined") {
+                el = $(this);
+            }
+
+            var rel = el.attr("rel");
+            var section = $("." + rel + "-section");
+            var top = section.offset().top;
+
+            $('body').animate({
+                scrollTop: top
+            });
+        }
+    };
+    nav.init();
+
+})(window, document, $, undefined);
